76b99f1035f9632ba45da7c8ac3b10900a209db4
ashutosh <kurvheashutosh@gmail.com>
2025-08-31 17:42:03 +0530

    Remove Duplicate Elements

---
+++ DSA/Array/Remove Ele/RemoveDuplicate.js
@@ -0,0 +1,15 @@
+function removeEle(nums) {
+   if (nums.length === 0) return 0;
+
+   let k = 1; // pointer for unique elements
+   for (let i = 1; i < nums.length; i++) {
+     if (nums[i] !== nums[i - 1]) {
+       nums[k] = nums[i]; // overwrite duplicate
+       k++;
+     }
+   }
+    //    return k;
+    console.log(k)
+}
+
+removeEle([1,1,2]);
---
+++ DSA/Array/Remove Ele/tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+ // let newArr = [];
+        // if (nums[i] !== nums[i - 1]) {
+        //     newArr.push(i)
+
+        // }
+        // for (let i = 0; i < newArr.length; i++){
+        //     nums.pop(newArr);
+        // }
