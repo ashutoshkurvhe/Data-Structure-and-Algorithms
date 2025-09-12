71e38d2404792e846a9ec715e648fe279cae29c4
ashutosh <kurvheashutosh@gmail.com>
2025-09-12 08:39:03 +0530

    Solid Rectangle

---
+++ 9 patterns/solidRect.js
@@ -0,0 +1,14 @@
+let num = 5;
+for (let i = 0; i < num; i++){
+    let row = "";
+    for (let j = 0; j < num; j++){
+        row += "* ";
+    }
+    console.log(row)
+}
+
+//i = 0 = * j = 0 to j = 4 -> * 5 time
+//i = 1 = * j = 0 to j = 4 -> * 5 time
+//i = 2 = * j = 0 to j = 4 -> * 5 time
+//i = 3 = * j = 0 to j = 4 -> * 5 time
+//i = 4 = * j = 0 to j = 4 -> * 5 time
